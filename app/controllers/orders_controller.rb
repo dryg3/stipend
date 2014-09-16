a8cc26b0f0fa3b94ee8ac92649f191924332656a
Verner Anna <vaa73@tower.msiu.ru>
2014-09-16 16:05:45 +0400

    шаблоны приказов

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,7 +12,8 @@ class OrdersController < ApplicationController
     @pays=PayCategoryToSemester.find(@order.pay_category_to_semester_id)
     @pay=PayToMonthStudent.where("month = ? AND year = ?",@pays.date_start.month, @pays.date_start.year)
     @student_groups=[]
-    @groups=Group.where('year = ? AND semester = ?', @order.year, @order.sem_today)
+    @groups=Group.where('year = ? AND semester = ? AND faculty_id = ?', @order.year, @order.semester,@order.faculty_id)
+    @groups=Group.where('year = ? AND semester = ? AND faculty_id = ? AND kurs = ?', @order.year, @order.semester,@order.faculty_id,1) if  @order.type_order==3
     groups=[]
     @groups.each { |g| groups<<g.id }
     @pay.each do |pay|
@@ -48,24 +49,28 @@ class OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     #raise params[:order][:list][6.to_s]["name1"].to_s.inspect
-    @order.bottom=""
-    arr=Array.new(8)
-    for i in 0...7
-      if i >= Order::LIST.size
-        arr[params[:order][:list][i.to_s]["id"].to_i]=params[:order][:list][i.to_s]["name1"]<<"\t"<<params[:order][:list][i.to_s]["name2"]<<"\n"
-      else
-        unless params[:order][:list][i.to_s]["id"]==""
-          arr[params[:order][:list][i.to_s]["id"].to_i]=Order::LIST[i][0].to_s<<"\t"<<Order::LIST[i][1].to_s<<"\n"
-        end
-      end
-     # raise arr.inspect
-    end
+    if @order.bottom.nil?
+      @order.bottom=""
+      arr=Array.new(params[:order][:list].size+1)
 
-    for i in 0...arr.size
-      @order.bottom<<arr[i] unless arr[i].nil?
+      for i in 0...params[:order][:list].size
+        if i >= Order::LIST.size
+          unless params[:order][:list][i.to_s]["id"]==""
+            arr[params[:order][:list][i.to_s]["id"].to_i]=params[:order][:list][i.to_s]["name1"]<<"\t"<<params[:order][:list][i.to_s]["name2"]<<"\n"
+          end
+        else
+          unless params[:order][:list][i.to_s]["id"]==""
+            arr[params[:order][:list][i.to_s]["id"].to_i]=Order::LIST[i][0].to_s<<"\t"<<Order::LIST[i][1].to_s<<"\n"
+          end
+        end
+       # raise arr.inspect
+      end
+
+      for i in 0...arr.size
+        @order.bottom<<arr[i] unless arr[i].nil?
+      end
+      #raise @order.bottom.inspect
     end
-    #raise @order.bottom.inspect
-    
     if @order.update(order_params)
       redirect_to action: 'index'
     else
